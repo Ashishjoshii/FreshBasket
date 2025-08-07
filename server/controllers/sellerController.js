2ff00bb48df3de98bb6590f6b614d0b988e673c5
Ashish joshii <joshiashish8888@gmail.com>
2025-08-07 13:52:28 +0530

    First Commit

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,51 @@
+// Login Seller : /api/seller/login
+import jwt from 'jsonwebtoken';
+
+export const sellerLogin = async (req, res) =>{
+   try {
+     const { email, password } = req.body;
+
+    if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL){
+        const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('sellerToken', token, {
+            httpOnly: true, //Prevent javaScript to access cookie
+            secure: process.env.NODE_ENV === 'production', //use secure cookie in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF Protection
+            maxAge: 7 * 24 * 60 * 60 * 1000,//Cookie expiration time
+        });
+        return res.json({ success: true, message: "Logged In"});
+    }else{
+        return res.json({ success: false, message: "Invalid Credentials" });
+    }
+   } catch (error) {
+     console.log(error.message);
+     res.json({ success: false, message: error.message });
+   }
+}
+
+//Seller Auth : /api/seller/is-auth
+export const isSellerAuth = async (req, res)=>{
+    try {
+        return res.json({ success: true})
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+//Logout User : /api/seller/logout
+
+export const sellerLogout = async (req, res)=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+        });
+        return res.json({ success: true, message: "Logged Out" })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
